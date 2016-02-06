204be663a627b0cb032988b18c82420e37710ee4
Kitlith <kitling@kitling.ktarian.com>
2016-02-05 21:44:58 -0800

    Fixed indentation, added comments and added a translation to a VAddr.

--- 3ds/src/main.c
+++ 3ds/src/main.c
@@ -4,8 +4,10 @@
 #include <brahma.h>
 
 #warning "This is a concept only! Everything is wrong!"
+// The following only works if IO is already in the MMU...
+#define REG_NTRCARDROMCNT (*(vu32*)(0x10164004 + 0xEB00000))
 
-#define REG_NTRCARDROMCNT (*(vu32*)0x10164004)
+// #shitcode time
 
 bool exit(void) {
     hidScanInput();
@@ -30,13 +32,13 @@ s32 priv_brahma_stuff(void) {
     __asm__ volatile ("cpsid aif"); // Copied from Brahma2
     u32 *save = (u32 *)(g_expdata.va_fcram_base + 0x3FFFE00);
     save[0] = topFramebufferInfo.framebuf0_vaddr;
-   	save[1] = topFramebufferInfo.framebuf1_vaddr;
-	save[2] = bottomFramebufferInfo.framebuf0_vaddr;
+    save[1] = topFramebufferInfo.framebuf1_vaddr;
+    save[2] = bottomFramebufferInfo.framebuf0_vaddr;
 
     // Working around a GCC bug to translate the va address to pa...
-	save[0] += 0xC000000;  // (pa FCRAM address - va FCRAM address)
-	save[1] += 0xC000000;
-	save[2] += 0xC000000;
+    save[0] += 0xC000000;  // (pa FCRAM address - va FCRAM address)
+    save[1] += 0xC000000;
+    save[2] += 0xC000000;
 
     map_arm9_payload();
     return 0;
@@ -56,7 +58,7 @@ int main(int argc, char **argv) {
 
     bool error = false;
     printf("Setting up payload area...");
-    brahma_init();
+    brahma_init(); // TODO: Actually check for error codes.
     load_arm9_payload_offset("/payload.bin", 0, 0);
     setup_exploit_data();
     svcBackdoor(priv_brahma_stuff);
